79c4ce3e2d2a1665c0197bc743ff44329610989f
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-06-05 17:27:11 +0300

    ghghghgh

--- js/script.js
+++ js/script.js
@@ -1,63 +1,58 @@
 // Створіть об'єкт "bankAccount" з властивостями "ownerName", "accountNumber", "balance". Додайте до об'єкту метод "deposit", який дозволяє додавати гроші на рахунок, та метод "withdraw", який дозволяє знімати гроші з рахунку. Використайте cofirm() щоб поповнити рахунок або отримати готівку та prompt() щоб дізнатися сумму. Після проведення операції виводити повідомлення про залишок на рахунку. 
 
-// const bankAccount = {
-//     ownerName: 'timofii',
-//     accountNumber: '1111 2222 3333 4444',
-//     balance: 100,
+const bankAccount = {
+    ownerName: 'timofii',
+    accountNumber: '1111 2222 3333 4444',
+    balance: 100,
 
 
-//     deposit(money) {
-//     this.balance += money
-//     },
+    deposit(money) {
+    this.balance += money
+    },
 
-//     withdraw(money) {
+    withdraw(money) {
 
-//      if (money <= this.balance) {
-//         this.balance -= money
-//      } else {
-//         alert('Недостатньо коштів!!!!')
-//      }
-//     },
+     if (money <= this.balance) {
+        this.balance -= money
+     } else {
+        alert('Недостатньо коштів!!!!')
+     }
+    },
 
     
-// }
-
-// bankAccount.withdraw(12)
-// console.log(bankAccount);
+}
 
 
-// // const result = confirm(sadasd)
-// // console.log(result);
-
-// // const res1 = prompt('dddd')
-// // const res2 = print('ddddd')
-// // console.log(res1+ res2);
+console.log(bankAccount);
 
 
 
-// if (confirm('ви бажаєте поповнити рахунок')) {
-
-// const userInput =  parseFloat(prompt('яку сумму хочете поповнити???'))
-// if (!isNaN(userInput) && userInput > 0 ) {
-// bankAccount.deposit(userInput)
-// } else {
-//     alert('не коректно введені данні')
-// }
 
 
-// }
 
-// else if (confirm('Ви бажаєте зняти кошти')){
+if (confirm('ви бажаєте поповнити рахунок')) {
 
-// const userInput =  parseFloat(prompt('яку сумму хочете зняти???'))
-// if (!isNaN(userInput) && userInput > 0 ) {
-// bankAccount.withdraw(userInput)
-// } else {
-//     alert('не коректно введені данні')
-// }
-// } else {
-//     alert('операція скасована')
-// }
+const userInput =  parseFloat(prompt('яку сумму хочете поповнити???'))
+if (!isNaN(userInput) && userInput > 0 ) {
+bankAccount.deposit(userInput)
+} else {
+    alert('не коректно введені данні')
+}
+
+
+}
+
+else if (confirm('Ви бажаєте зняти кошти')){
+
+const userInput =  parseFloat(prompt('яку сумму хочете зняти???'))
+if (!isNaN(userInput) && userInput > 0 ) {
+bankAccount.withdraw(userInput)
+} else {
+    alert('не коректно введені данні')
+}
+} else {
+    alert('операція скасована')
+}
 
 // Створіть об'єкт "weather" з властивостями "temperature", "humidity", "windSpeed". Додайте до об'єкту метод, який повертає "true", якщо температура нижче 0 градусів Цельсія, та "false", якщо температура вище або рівна 0 градусів Цельсія. Температуру потрібно отримати з prompt(). Якщо метод повернув "true" вивести повідомлення “температура нижче 0 градусів Цельсія” і навпаки
 
@@ -83,22 +78,34 @@ if (weather.termometer() ) {
 
 
 const user = {
-    name: 'timofii',
-    email: "asdasdasda@gmail.com",
-    password: 'qwerty',
-    
-    login(newEmail, newPassword) {
-        if (this.email === newEmail && this.password === newPassword) {
-            mesage = 'Ви успішно увійшли до свого акаунту';
-            
-        } else {
-            mesage = 'Ви ввели не коректні данні'
-        }
+  name: "Тимофій",
+  email: "timofii@example.com",
+  password: "qwe123",
 
-        return mesage
+  login: function(inputEmail, inputPassword) {
+    if (this.email === inputEmail && this.password === inputPassword) {
+      console.log("Вхід успішний!");
+      
+    } else {
+      console.log("Невірний email або пароль.");
+      
     }
+  }
+};
 
-}
 
-console.log(user.login("asdasdasda@gmail.com", 'qwerty'));
+user.login("timofii@example.com", "qwe123"); 
+user.login("ivan@example.com", "wrongpass");
 
+const movie = {
+title: "Початок",
+director: "Тимофій",
+year: 2010,
+rating: 7,
+ };
+
+
+
+
+const isHighlyRated = movie.rating > 8;
+ console.log("Високий рейтинг?", isHighlyRated); 
